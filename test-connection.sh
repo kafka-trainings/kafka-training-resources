278c540eadd9d29a3f806f9c23b6d8b9ebddcdea
Anatoly Zelenin <anatoly@zelenin.de>
2021-01-28 15:19:37 +0100

    Improve test-connection

--- test-connection.sh
+++ test-connection.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
-set -e
 echo "Testing Zookeeper…"
 echo -n "ZK 1"
-zookeeper-shell.sh localhost:2181 ls / > /dev/null
+zookeeper-shell.sh localhost:2181 ls / > /dev/null 2>&1 || echo -ne "\t !!! Not"
 echo -e "\tOK"
 echo -n "ZK 2"
-zookeeper-shell.sh localhost:2182 ls / > /dev/null
+zookeeper-shell.sh localhost:2182 ls / > /dev/null 2>&1 || echo -ne "\t !!! Not"
 echo -e "\tOK"
 echo -n "ZK 3"
-zookeeper-shell.sh localhost:2183 ls / > /dev/null
+zookeeper-shell.sh localhost:2183 ls / > /dev/null 2>&1 || echo -ne "\t !!! Not"
 echo -e "\tOK"
 echo "Testing Kafka…"
 echo -n "Kafka 1"
-kafka-broker-api-versions.sh --bootstrap-server localhost:9092 > /dev/null
+kafka-broker-api-versions.sh --bootstrap-server localhost:9092 > /dev/null 2>&1 || echo -ne "\t !!! Not"
 echo -e "\tOK"
 echo -n "Kafka 2"
-kafka-broker-api-versions.sh --bootstrap-server localhost:9093 > /dev/null
+kafka-broker-api-versions.sh --bootstrap-server localhost:9093 > /dev/null 2>&1 || echo -ne "\t !!! Not"
 echo -e "\tOK"
 echo -n "Kafka 3"
-kafka-broker-api-versions.sh --bootstrap-server localhost:9094 > /dev/null
+kafka-broker-api-versions.sh --bootstrap-server localhost:9094 > /dev/null 2>&1 || echo -ne "\t !!! Not"
 echo -e "\tOK"
 echo "Happy Hacking!"
