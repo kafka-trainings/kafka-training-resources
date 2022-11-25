4e0b105f8fb11142a744e967fa002a6db6e6e8fa
Anatoly Zelenin <anatoly@zelenin.de>
2022-11-25 16:01:22 +0100

    Update Kafka

--- download-and-install-kafka.sh
+++ download-and-install-kafka.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-KAFKA_VERSION="2.13-3.1.0"
-wget "https://downloads.apache.org/kafka/3.1.0/kafka_${KAFKA_VERSION}.tgz"
+KAFKA_VERSION="2.13-3.3.1"
+wget "https://downloads.apache.org/kafka/3.3.1/kafka_${KAFKA_VERSION}.tgz"
 tar xfz kafka_${KAFKA_VERSION}.tgz
 rm kafka_${KAFKA_VERSION}.tgz
 mv kafka_${KAFKA_VERSION} ~/kafka
