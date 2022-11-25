dbfe5cc347f987737f6cafe7d5a21e8a163cf33d
Anatoly Zelenin <anatoly@zelenin.de>
2022-11-25 21:54:34 +0100

    Bugfixes

--- start-kafka-env.sh
+++ start-kafka-env.sh
@@ -4,17 +4,17 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 if [[ ! -d "/home/user/training/data/kafka1" ]]; then
   KAFKA_CLUSTER_ID="$(kafka-storage.sh random-uuid)"
-  kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c "$DIR"/config/kafka1.properties
-  kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c "$DIR"/config/kafka2.properties
-  kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c "$DIR"/config/kafka3.properties
+  kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c /home/user/training/config/kafka1.properties
+  kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c /home/user/training/config/kafka2.properties
+  kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c /home/user/training/config/kafka3.properties
 fi
 
 export KAFKA_OPTS=" -javaagent:$DIR/javaagent.jar=8081:$DIR/config/kafka-javaagent.yaml"
-kafka-server-start.sh -daemon "$DIR"/config/kafka1.properties
+kafka-server-start.sh -daemon /home/user/training/config/kafka1.properties
 export KAFKA_OPTS=" -javaagent:$DIR/javaagent.jar=8082:$DIR/config/kafka-javaagent.yaml"
-kafka-server-start.sh -daemon "$DIR"/config/kafka2.properties
+kafka-server-start.sh -daemon /home/user/training/config/kafka2.properties
 export KAFKA_OPTS=" -javaagent:$DIR/javaagent.jar=8083:$DIR/config/kafka-javaagent.yaml"
-kafka-server-start.sh -daemon "$DIR"/config/kafka3.properties
+kafka-server-start.sh -daemon /home/user/training/config/kafka3.properties
 unset KAFKA_OPTS
 echo "Kafka started. Waiting 15s to finish booting"
 sleep 15
