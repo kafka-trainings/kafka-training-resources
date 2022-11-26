46e87f671ebe3c11f19b887fe778bdaae9c13352
Anatoly Zelenin <anatoly@zelenin.de>
2022-11-26 16:43:02 +0100

    Bugfixes

--- start-kafka-env.sh
+++ start-kafka-env.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 if [[ ! -d "/home/user/training/data/kafka1" ]]; then
   KAFKA_CLUSTER_ID="$(kafka-storage.sh random-uuid)"
@@ -9,11 +8,11 @@ if [[ ! -d "/home/user/training/data/kafka1" ]]; then
   kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c /home/user/training/config/kafka3.properties
 fi
 
-export KAFKA_OPTS=" -javaagent:$DIR/javaagent.jar=8081:$DIR/config/kafka-javaagent.yaml"
+export KAFKA_OPTS=" -javaagent:/home/user/training/javaagent.jar=8081:/home/user/training/config/kafka-javaagent.yaml"
 kafka-server-start.sh -daemon /home/user/training/config/kafka1.properties
-export KAFKA_OPTS=" -javaagent:$DIR/javaagent.jar=8082:$DIR/config/kafka-javaagent.yaml"
+export KAFKA_OPTS=" -javaagent:/home/user/training/javaagent.jar=8082:/home/user/training/config/kafka-javaagent.yaml"
 kafka-server-start.sh -daemon /home/user/training/config/kafka2.properties
-export KAFKA_OPTS=" -javaagent:$DIR/javaagent.jar=8083:$DIR/config/kafka-javaagent.yaml"
+export KAFKA_OPTS=" -javaagent:/home/user/training/javaagent.jar=8083:/home/user/training/config/kafka-javaagent.yaml"
 kafka-server-start.sh -daemon /home/user/training/config/kafka3.properties
 unset KAFKA_OPTS
 echo "Kafka started. Waiting 15s to finish booting"
--- stop-kafka-env.sh
+++ stop-kafka-env.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 set -e
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-zookeeper-server-stop.sh "$DIR"/config/zk1.properties
-zookeeper-server-stop.sh "$DIR"/config/zk2.properties
-zookeeper-server-stop.sh "$DIR"/config/zk3.properties
-echo "Zookeeper stopped."
-kafka-server-stop.sh "$DIR"/config/kafka1.properties
-kafka-server-stop.sh "$DIR"/config/kafka2.properties
-kafka-server-stop.sh "$DIR"/config/kafka3.properties
+kafka-server-stop.sh /home/user/training/config/kafka1.properties
+kafka-server-stop.sh /home/user/training/config/kafka2.properties
+kafka-server-stop.sh /home/user/training/config/kafka3.properties
 echo "Kafka stopped."
