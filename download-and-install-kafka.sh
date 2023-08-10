49418b3fa1368c5d2de1358debca4ca9d630a3c2
Anatoly Zelenin <anatoly@zelenin.de>
2023-08-10 15:50:58 +0200

    Fix Kafka URL

--- download-and-install-kafka.sh
+++ download-and-install-kafka.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-KAFKA_VERSION="2.13-3.4.0"
-wget "https://downloads.apache.org/kafka/3.4.0/kafka_${KAFKA_VERSION}.tgz"
-tar xfz kafka_${KAFKA_VERSION}.tgz
-rm kafka_${KAFKA_VERSION}.tgz
-mv kafka_${KAFKA_VERSION} ~/kafka
+SCALA_VERSION="2.13"
+KAFKA_VERSION="3.4.0"
+FULL_VERSION="${SCALA_VERSION}-${KAFKA_VERSION}"
+wget "https://archive.apache.org/dist/kafka/${KAFKA_VERSION}/kafka_${FULL_VERSION}.tgz"
+tar xfz kafka_${FULL_VERSION}.tgz
+rm kafka_${FULL_VERSION}.tgz
+mv kafka_${FULL_VERSION} ~/kafka
 export PATH=~/kafka/bin:"$PATH"
 echo "export PATH=~/kafka/bin:$PATH" >> ~/.bashrc
