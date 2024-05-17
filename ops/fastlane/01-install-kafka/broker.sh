78b41be256185d74c6356ca2278f112e928dfd61
Anatoly Zelenin <anatoly@zelenin.de>
2024-05-17 15:31:54 +0000

    Fastlane for Ops

---
+++ ops/fastlane/01-install-kafka/broker.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Cluster ID is the first argument
+KAFKA_CLUSTER_ID=$1
+# If the cluster ID is not provided return an error
+if [ -z "$KAFKA_CLUSTER_ID" ]; then
+  echo "Cluster ID is required"
+  exit 1
+fi
+
+mkdir -p ~/kafka-data ~/config
+cd ~/config
+cp ~/kafka/config/server.properties ~/config/kafka.properties
+ID=$(hostname | grep -oE '[0-9]+$')
+
+cat <<EOF >> ~/config/kafka.properties
+log.dirs=/home/user/kafka-data
+process.roles=broker,controller
+controller.listener.names=CONTROLLER
+listener.security.protocol.map=CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT
+controller.quorum.voters=1@$IP.1:9192,2@$IP.2:9192,3@$IP.3:9192
+
+broker.id=$ID
+listeners=PLAINTEXT://$IP.$ID:9092,CONTROLLER://$IP.$ID:9192
+EOF
+
+kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c ~/config/kafka.properties
+kafka-server-start.sh -daemon ~/config/kafka.properties
---
+++ ops/fastlane/01-install-kafka/jumphost.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo "Updating known hosts…"
+ssh-keyscan -H "$IP".1 >> ~/.ssh/known_hosts
+ssh-keyscan -H "$IP".2 >> ~/.ssh/known_hosts
+ssh-keyscan -H "$IP".3 >> ~/.ssh/known_hosts
+
+echo "Updating git repos on the brokers…"
+ssh "$IP".1 "cd /home/user/training/ && git pull"
+ssh "$IP".2 "cd /home/user/training/ && git pull"
+ssh "$IP".3 "cd /home/user/training/ && git pull"
+
+# Check if cluster_id is already defined in file ~/CLUSTER_ID
+# If not generate it
+if [ -f ~/CLUSTER_ID ]; then
+  echo "Cluster ID already exists…"
+  CLUSTER_ID=$(cat ~/CLUSTER_ID)
+else
+echo "Generating cluster ID…"
+  CLUSTER_ID=$(kafka-storage.sh random-uuid)
+  echo $CLUSTER_ID > ~/CLUSTER_ID
+fi
+echo "Cluster ID: $CLUSTER_ID"
+
+echo "Configuring Kafka on the brokers…"
+ssh "$IP".1 "/home/user/training/ops/fastlane/01-install-kafka/broker.sh $CLUSTER_ID"
+ssh "$IP".2 "/home/user/training/ops/fastlane/01-install-kafka/broker.sh $CLUSTER_ID"
+ssh "$IP".3 "/home/user/training/ops/fastlane/01-install-kafka/broker.sh $CLUSTER_ID"
