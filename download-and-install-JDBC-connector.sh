cb2590e3566f566e5d05234743e1f5678c976030
Anatoly Zelenin <anatoly@zelenin.de>
2024-07-25 12:11:46 +0000

    Fixes

--- download-and-install-JDBC-connector.sh
+++ download-and-install-JDBC-connector.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-wget "https://d1i4a15mxbxib1.cloudfront.net/api/plugins/confluentinc/kafka-connect-jdbc/versions/10.0.1/confluentinc-kafka-connect-jdbc-10.0.1.zip"
-unzip confluentinc-kafka-connect-jdbc-10.0.1.zip
-rm confluentinc-kafka-connect-jdbc-10.0.1.zip
+wget "https://d2p6pa21dvn84.cloudfront.net/api/plugins/confluentinc/kafka-connect-jdbc/versions/10.7.6/confluentinc-kafka-connect-jdbc-10.7.6.zip"
+unzip confluentinc-kafka-connect-jdbc-10.7.6.zip
+rm confluentinc-kafka-connect-jdbc-10.7.6.zip
 mkdir java
-mv confluentinc-kafka-connect-jdbc-10.0.1/ java/
+mv confluentinc-kafka-connect-jdbc-10.7.6/ java/
