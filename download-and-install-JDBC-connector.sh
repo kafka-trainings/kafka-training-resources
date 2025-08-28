7faa78a938e7fb47990b99d0486036bd40d728a3
Anatoly Zelenin <anatoly@zelenin.de>
2025-08-28 11:47:27 +0200

    Make it idempotent

--- download-and-install-JDBC-connector.sh
+++ download-and-install-JDBC-connector.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
-wget "https://d2p6pa21dvn84.cloudfront.net/api/plugins/confluentinc/kafka-connect-jdbc/versions/10.7.6/confluentinc-kafka-connect-jdbc-10.7.6.zip"
-unzip confluentinc-kafka-connect-jdbc-10.7.6.zip
-rm confluentinc-kafka-connect-jdbc-10.7.6.zip
-mkdir java
-mv confluentinc-kafka-connect-jdbc-10.7.6/ java/
+
+# Version of the JDBC connector to install
+VERSION="10.7.6"
+
+# Check if the JDBC connector already exists
+if [ -d "java/confluentinc-kafka-connect-jdbc-${VERSION}" ]; then
+    echo "JDBC connector already exists in java/confluentinc-kafka-connect-jdbc-${VERSION}/"
+    echo "Skipping installation..."
+    exit 0
+fi
+
+# Only execute if the connector doesn't exist
+wget "https://d2p6pa21dvn84.cloudfront.net/api/plugins/confluentinc/kafka-connect-jdbc/versions/${VERSION}/confluentinc-kafka-connect-jdbc-${VERSION}.zip"
+unzip "confluentinc-kafka-connect-jdbc-${VERSION}.zip"
+rm "confluentinc-kafka-connect-jdbc-${VERSION}.zip"
+mkdir -p java
+mv "confluentinc-kafka-connect-jdbc-${VERSION}/" java/
